a8ebaa9442c8820be2c2855f0433126aabe7bcae
prashantvik <prashantvikram050599@gmail.com>
2023-08-22 20:53:39 +0530

    Pushing codes, docs in pvp_dev branch

---
+++ docs/Project Workflow Details.sql
@@ -0,0 +1,29 @@
+--STOCK DATA API KEY : FREE API KEY :  4TDQ3TAK1A574B32
+
+Free API Key, diff account. Diff requests per account
+different api for diff data
+No of requests over, change account api key
+
+Data Transformation and Cleaning
+According to data, for every 50 api call or so, store all the data after converting it into a bit structure to a file
+After some fix amount of api calls, clean the data and transform
+Save the raw data file and transformed data file
+Write ETL Job that run on the file generation trigger 
+
+Kafka data streaming
+For every transformed file, stream the data from this file to 
+
+
+Data Store - RDMS
+Data Modelling
+Postrgres or MSSQL;
+
+
+WRITE SQL 
+Do analytics 
+
+
+Power BI Reports 
+Modelling, DAX, Measure
+Multiple dashboards and reports 
+
